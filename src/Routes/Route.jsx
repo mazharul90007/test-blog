a871108af85f9009b7aa0b1b23b23b360574b962
MahdiHassanGo <contact.mahdiasif@gmail.com>
2025-03-10 23:51:57 +0600

    added blog

--- src/Routes/Route.jsx
+++ src/Routes/Route.jsx
@@ -2,22 +2,27 @@ import { createBrowserRouter } from "react-router-dom";
 import HomePage from "../Pages/Home/Home";
 import MainLayout from "../Pages/LayOut/MainLayout";
 import About from "../Pages/About/About";
+import Blog from "../Pages/Blog/Blog";
 
 const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <MainLayout></MainLayout>,
-      children: [
-        {
-            path: '/',
-            element: <HomePage></HomePage>
-        },
-        {
-            path: '/about',
-            element: <About/>
-        },
-      ]
-    },
-  ]);
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    children: [
+      {
+        path: "/",
+        element: <HomePage></HomePage>,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/blog",
+        element: <Blog />,
+      },
+    ],
+  },
+]);
 
 export default router;
