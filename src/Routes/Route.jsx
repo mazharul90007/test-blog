00734608dea3a4fbdd1dfff81a829a38f6d9d3ba
mazharul90007 <mazharul90007@gmail.com>
2025-03-11 01:00:01 +0600

    Route page updated

--- src/Routes/Route.jsx
+++ src/Routes/Route.jsx
@@ -2,29 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import HomePage from "../Pages/Home/Home";
 import MainLayout from "../Pages/LayOut/MainLayout";
 import About from "../Pages/About/About";
-
 import Blog from "../Pages/Blog/Blog";
-
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <MainLayout></MainLayout>,
-//     children: [
-//       {
-//         path: "/",
-//         element: <HomePage></HomePage>,
-//       },
-//       {
-//         path: "/about",
-//         element: <About />,
-//       },
-//       {
-//         path: "/blog",
-//         element: <Blog />,
-//       },
-//     ],
-//   },
-// ]);
 import Contact from "../Pages/Contact/Contact";
 import Gallery from "../Pages/Gallery/Gallery";
 
