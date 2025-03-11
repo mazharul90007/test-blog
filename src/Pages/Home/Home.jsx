ff29bae57876df17980da489d843e95f6ddfb6b8
Tonmoy <tonmoyhosen767@gmail.com>
2025-03-12 00:00:04 +0600

    homepage redesign

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,11 @@
-
-
 const HomePage = () => {
-    return (
-        <div>
-            <h2 className="text-3xl font-semibold text-amber-700 text-center ">Hello Dear. This is Home Page</h2>
-        </div>
-    );
+  return (
+    <div>
+      <h2 className="text-3xl font-semibold text-amber-700 text-center ">
+        Hello Dear. This is Home Page
+      </h2>
+    </div>
+  );
 };
 
-export default Home;
+export default HomePage;
